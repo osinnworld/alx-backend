47dfa8be8b066e085643675a2671e91d975c10fa
osinnworld <shemogata@gmail.com>
2024-07-30 18:31:11 +0000

    revision 4

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python3
-""" 2-appy: add locale """
-
-from flask import Flask, render_template, request
+""" 2-app module """
+from flask import Flask, request
 from flask_babel import Babel
+from routes.routes_2 import app_routes
+from config import Config
+
 
 app = Flask(__name__)
 babel = Babel(app)
 
-
-class Config:
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app.config.from_object(Config)
+app.register_blueprint(app_routes)
 
 
 @babel.localeselector
-def get_locale():
-    """ determine the best match with our supported languages. """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale() -> str:
+    """ Determine best match for supported languages
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-@app.route('/')
-def index():
-    """ Create / route """
-    return render_template('2-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
