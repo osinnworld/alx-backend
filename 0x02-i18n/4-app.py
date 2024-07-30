14356282ebded4ee42525fea7c8e342d47905abc
osinnworld <shemogata@gmail.com>
2024-07-30 18:45:01 +0000

    4-app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Parametrize templates
+Force locale with URL parameter
 """
 
 import babel
@@ -26,8 +26,12 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """
-     determine the best match with our supported languages.
+    detect if the incoming request contains locale
+    argument and ifs value is a supported locale, return it
     """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
@@ -36,7 +40,7 @@ def index():
     """
     hello world
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
